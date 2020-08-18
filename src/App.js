75ad287f65120313edbcd608f580d634f47113cb
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-08-18 19:22:06 -0400

    ok

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const fetchData = () => {
+  fetch('./public/contracts.json')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+})
+}
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <h1>hi
+    {fetchData()}
+  </h1>
+  )
 }
 
 export default App;
